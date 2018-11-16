62d3f2a65a52789de4b43ebc875de111113cff03
BlackVS <vvs@coders.in.ua>
2018-11-16 12:48:57 +0200

    output fixed

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+sudo echo Installing Telegram bot
+
 BOTDIR=tgbot
 INSTALLDIR=/opt/$BOTDIR
 
+WHITE='\033[1;37m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[1;32m'
 NC='\033[0m'
+
+BOLD="\e[1m"
+NOBOLD="\e[21m"
 
 onerror() {
     echo " FAILED, aborting script"
@@ -30,10 +36,16 @@ function run() {
     check
 }
 
+function print_h0(){
+    echo -e "* ${WHITE}${BOLD} $@  ${NOBOLD}${NC}:"
+}
+
 function print_h1(){
     echo ""
     echo -n -e "* ${YELLOW} $@  ${NC}:"
 }
+
+print_h0 "Installing Telegram bot"
 
 echo "Install bot to: $INSTALLDIR"
 
@@ -49,7 +61,7 @@ print_h1 "switching to dev"
 run sudo git checkout dev
 
 print_h1 "installing python3-pip"
-run sudo apt -q install python3-pip -y
+run sudo apt -qqq install python3-pip -y
 
 print_h1 "installing pipenv"
 run sudo pip3 -q install pipenv 
