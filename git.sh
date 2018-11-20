f0124d1d7f142fc443e036435720e97dbc5f184b
BlackVS <vvs@coders.in.ua>
2018-11-20 10:43:08 +0200

    agnostic fixed

--- git.sh
+++ git.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-UNAME="BlackVS"
-EMAIL="vvs@polesye.com"
 
-sudo apt -y install git-core zsh
-sudo apt -y install fonts-powerline
+#sudo apt update
+#sudo apt -y upgrade
+
+read -p 'GIT Username: ' UNAME
+read -p 'GIT EMAIL: ' EMAIL
+
+sudo apt -y install git-core fonts-powerline zsh
 
 cmd="wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O -"
 #for current user
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo $cmd
 $cmd | zsh
 
 #for root
@@ -15,14 +19,14 @@ $cmd | zsh
 sudo su -c "$cmd | zsh"
 
 cmd="git config --global user.name $UNAME"
-#echo $cmd
+echo $cmd
 $cmd
 sudo su -c "$cmd"
 
 cmd="git config --global user.email $EMAIL"
-#echo $cmd
+echo $cmd
 $cmd
 sudo su -c "$cmd"
 
-perl -i -pe 's!^(ZSH_THEME\s*=\s*).*!\1"agnostic"!; s!^(plugins\s*=[\s\n]*\([\s\n]*).*!\1 gitfast!' ~/.zshrc
+perl -i -pe 's!^(ZSH_THEME\s*=\s*).*!\1"agnoster"!; s!^(plugins\s*=[\s\n]*\([\s\n]*).*!\1 gitfast!' ~/.zshrc
 #chsh -s $(which zsh)
